1486a09ed3d2ab0a75e977ce2da61f914ddaa9c1
SERVER20\js-20567246 <incrediblemaybell9@gmail.com>
2023-10-07 15:45:43 +0800

    Cardenas

---
+++ ASI.Basecode.WebApp/Controllers/Cardenas.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASI.Basecode.WebApp.Controllers
+{
+    public class Cardenas : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
